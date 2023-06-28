6a5d98a137845128233606c096029e8d4fc080f8
kalfian <hi@kalfian.com>
2023-06-29 02:59:50 +0700

    Add custom css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Custom styles for this template-->
     <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
     @yield('css')
 </head>
 
