0f19937e9bd2afdee74f839fda5a60b21f26b286
kalfian <hi@kalfian.com>
2023-10-21 16:17:09 +0700

    support get by user id when is lecturer

--- app/Http/Controllers/V1/LogbookController.php
+++ app/Http/Controllers/V1/LogbookController.php
@@ -16,6 +16,11 @@ class LogbookController extends Controller
     public function index(Activity $activity, Request $request) {
         $user = $request->user();
 
+        $userId = $request->user_id ?? $user->id;
+        if(!$user->hasRole('lecture')) {
+            $userId = $user->id;
+        }
+
         // Validate if user is participant
         $isParticipant = $activity->participants->where('user_id', $user->id)->first();
         if (!$isParticipant) {
@@ -27,7 +32,48 @@ class LogbookController extends Controller
         $user = $request->user();
         $limit = $request->limit ?? 10;
 
-        $logbooks = ActivityLogbook::where('user_id', $user->id)
+        $logbooks = ActivityLogbook::where('user_id', $userId)
+            ->where('activity_id', $activity->id)
+            ->orderBy('date', 'desc')
+            ->paginate($limit);
+
+        $collection = LogbookResource::collection($logbooks);
+
+        $meta = [
+            'total' => $logbooks->total(),
+            'current_page' => $logbooks->currentPage(),
+            'per_page' => $logbooks->perPage(),
+            'last_page' => $logbooks->lastPage()
+        ];
+
+        return response()->json([
+            'message' => 'Berhasil menampilkan data',
+            'data' => $collection,
+            'meta' => $meta,
+        ], 200);
+    }
+
+    public function indexByUserId(Activity $activity, Request $request) {
+        // Validate
+        $rules = [
+            'student_id' => ['required', 'integer', 'exists:users,id']
+        ];
+
+        $user = $request->user();
+        $studentId = $request->student_id;
+
+        // Validate if user is participant
+        $isParticipant = $activity->participants->where('user_id', $user->id)->first();
+        if (!$isParticipant) {
+            return response()->json([
+                'message' => 'Anda tidak memiliki akses kegiatan ini',
+            ], 401);
+        }
+
+        $user = $request->user();
+        $limit = $request->limit ?? 10;
+
+        $logbooks = ActivityLogbook::where('user_id', $studentId)
             ->where('activity_id', $activity->id)
             ->orderBy('date', 'desc')
             ->paginate($limit);
@@ -64,6 +110,8 @@ class LogbookController extends Controller
             'data' => new LogbookResource($logbook),
         ], 200);
     }
+
+
 
     public function store(Activity $activity, Request $request) {
         $user = $request->user();
